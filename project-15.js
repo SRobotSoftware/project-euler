9679a9c12156ca63e2c2ed487bd15db581601572
Zmixon91 <zmixon91@gmail.com>
2016-06-20 21:00:13 -0600

    Actually still working on 15

--- project-15.js
+++ project-15.js
@@ -1,3 +1,13 @@
 // Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
 // How many such routes are there through a 20×20 grid?
+
+function buildNodes(x) {
+	var out = new Array(x)
+	var nodes = new Array(x)
+	nodes.fill(2)
+	nodes[nodes.length - 1] -= 1
+	out.fill(nodes)
+	return out
+}
+console.log(buildNodes(3))
